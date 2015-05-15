2004389a46c18311ea6f7d409bde7af3b8185eb0
Saurav Dan <sauravdan@yahoo.com>
2015-05-15 14:59:33 +0530

    Update venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/vagrant/flask-oauth-example/venv"
+set -gx VIRTUAL_ENV "/home/saurav/zearch/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
